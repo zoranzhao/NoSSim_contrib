b7cde58cac86059cb3f50d9d3f076af6845d64a1
Zhuoran Zhao <zoranzhao@gmail.com>
2018-04-18 20:35:01 -0500

    First check in

---
+++ 6LoWPAN/src/NoS/applications/OSNode/AppTasks.h
@@ -0,0 +1,121 @@
+/*********************************************                                                       
+ * VisionGraph Tasks Based on Interrupt-Driven Task Model                                                                     
+ * Zhuoran Zhao, UT Austin, zhuoran@utexas.edu                                                    
+ * Last update: July 2016                                                                            
+ ********************************************/
+#include <omnetpp.h>
+#include <systemc>
+
+#ifndef SC_VISION_TASK__H
+#define SC_VISION_TASK__H
+
+#define CLI_NUM_MAX 10
+#define CLI_NUM 6
+#define IMG_NUM 4
+extern unsigned long cli_commu[CLI_NUM_MAX];
+extern int schd[CLI_NUM_MAX];
+extern double CliEnergy[10];
+
+void cli_wait(int NodeID);
+void srv_wait(int NodeID);
+void srv_send(int NodeID, int cliID);
+void cli_send(int NodeID, int cliID);
+void srv_send_dat(int NodeID,  int from_cliID, int cliID,  int image_count);
+
+
+
+
+class IntrDriven_Task :public sc_core::sc_module
+{
+ public:
+
+
+    sc_core::sc_port< sc_core::sc_fifo_out_if<int> > size_out;
+    sc_core::sc_port< sc_core::sc_fifo_in_if<int> > size_in;
+
+    sc_core::sc_port< sc_core::sc_fifo_out_if<char*> > data_out;
+    sc_core::sc_port< sc_core::sc_fifo_in_if<char*> > data_in;
+
+
+    SC_HAS_PROCESS(IntrDriven_Task);
+    IntrDriven_Task(const sc_core::sc_module_name name, int NodeID)
+    :sc_core::sc_module(name)
+    {
+	this->NodeID = NodeID;
+        SC_THREAD(run_jobs);
+
+    }
+    
+    ~IntrDriven_Task() {}
+
+
+
+
+
+
+
+ private:
+	int NodeID;
+	char* data_send;
+	int size_send;
+	char* data_recv;
+	int size_recv;
+	//int cli_send_pkt_number[CLI_NUM_MAX];
+
+
+	void recv_imgs()
+	{
+
+
+
+	  while(1){
+
+
+
+
+		size_recv = size_in -> read();
+		data_recv = data_in -> read();
+
+
+
+	  }//while(1)
+	}
+
+
+
+	void send_imgs()
+	{
+
+	
+	  for(int i = 0; i < (IMG_NUM*1000); i++){//Client running times 
+
+
+
+		size_send = 100;
+		data_send = (char *)malloc(size_send);
+		data_send[0]='e';
+		data_send[1]='n';
+		data_send[2]='d';
+		size_out -> write(size_send);
+		data_out -> write(data_send);
+
+		//Do something here ... ... ...
+
+	  }
+	}
+
+
+	void run_jobs(void)
+	{
+	   if(NodeID == 0)//server
+	   	recv_imgs();
+	   else
+		send_imgs();
+	   
+	}
+
+
+
+};
+
+#endif // SC_VISION_TASK__H 
---
+++ 6LoWPAN/src/NoS/applications/OSNode/Nic.h
@@ -0,0 +1,178 @@
+/*********************************************
+ * Network Interface Card Model
+ * Zhuoran Zhao, UT Austin, zhuoran@utexas.edu
+ * Last update: July 2015
+ ********************************************/
+
+#include <string.h>
+
+#include "OmnetIf_pkt.h"
+#include <omnetpp.h>
+#include <systemc>
+
+using namespace omnetpp;
+
+using namespace ::sc_core;
+using namespace ::std;
+
+#ifndef SC_NIC__H
+#define SC_NIC__H
+
+class Nic : public sc_module {
+   public:
+
+	sc_core::sc_port< sc_fifo_out_if<int> > size_out;
+	sc_core::sc_port< sc_fifo_in_if<int> > size_in;
+
+        sc_core::sc_port< sc_fifo_out_if<char*> > data_out;
+        sc_core::sc_port< sc_fifo_in_if<char*> > data_in;
+
+
+	int NodeID;
+   	sc_event recvd; 
+   	sc_event sent; 
+   	int recvd_flag; 
+   	int sent_flag; 
+
+
+	char* data_send;
+	int size_send;
+	char* data_recv;
+	int size_recv;
+	cSimpleModule* OmnetWrapper;
+
+
+
+
+	SC_HAS_PROCESS(Nic);
+
+	Nic(sc_module_name name, int NodeID) : sc_module(name)
+	{
+		recvd_flag = 0; 
+   		sent_flag = 0; 
+		this -> NodeID = NodeID;
+		SC_THREAD(NicRecv);
+		SC_THREAD(NicSend);
+	}
+
+	void NicSend()
+	{
+	  while (1) {
+		size_send = size_in -> read();
+		data_send = data_in -> read();
+
+
+    	        //std::cout << "Client sends images ... ... :::::" << NodeID <<std::endl;
+		cSimpleModule* wrapper = (cSimpleModule*)(OmnetWrapper);
+		cContextSwitcher dummy1(wrapper); //VERY IMPORTANT
+		OmnetIf_pkt* pkt = new OmnetIf_pkt();
+		pkt->setFileBufferArraySize(size_send);
+		for(int ii=0; ii<size_send; ii++){
+				pkt->setFileBuffer(ii, ((char*)data_send)[ii]);
+		}
+    	        //std::cout << "Client sends images ... ... :::::" << NodeID <<std::endl;
+		free(data_send);
+    	        //std::cout << "Client sends images ... ... :::::" << NodeID <<std::endl;
+		cMessage *startMsg = new cMessage("ServerToCli");
+		startMsg->setContextPointer(pkt);
+		wrapper->scheduleAt(simTime(), startMsg);  //Notify immediately
+		wait(this->sent);
+	  }
+		
+/*
+		int pkt_total=100;
+		int pkt_count=0;
+		if(NodeID!=0 && (strcmp("nic1", this->basename())==0)){
+
+		  for(pkt_count=0; pkt_count<pkt_total; pkt_count++){
+			//size_send = size_in -> read();
+			//data_send = data_in -> read();
+		        std::cout << " NicSend 1... ... ... " <<std::endl;
+			//Generating data 
+			//Sending packets out to MAC layer
+
+			size_send = 500;
+			data_send = (char *)malloc(size_send);
+			//for(int ii=0; ii<size_send; ii++)
+			//	data_send[ii] = ii;
+
+
+			cSimpleModule* wrapper = (cSimpleModule*)(OmnetWrapper);
+			cContextSwitcher dummy1(wrapper); //VERY IMPORTANT
+			OmnetIf_pkt* pkt = new OmnetIf_pkt();
+			pkt->setFileBufferArraySize(size_send);
+			    for(int ii=0; ii<size_send; ii++){
+				pkt->setFileBuffer(ii, ((char*)data_send)[ii]);
+			    }
+
+			free(data_send);
+
+			cMessage *startMsg = new cMessage("ServerToCli");
+			startMsg->setContextPointer(pkt);
+			wrapper->scheduleAt(simTime(), startMsg);  //Notify immediately
+			//std::cout << "sc_core::wait(sent); time is: " << sc_time_stamp().value() << std::endl;
+	   		//if (sent_flag==0) 
+			wait(this->sent);
+			//std::cout << "sc_core::after_ wait(sent); time is: " << sc_time_stamp().value() << std::endl;
+			//sent_flag--;			
+		  }
+		}
+*/
+	}
+
+	void NicRecv()
+	{
+
+		int recv_count = 0;
+		while (1) {
+			//std::cout << "sc_core::wait(recvd); time is: " << sc_time_stamp().value() << std::endl;
+			wait(recvd);
+			recv_count++;
+			if(NodeID==0){
+				std::cout << "recv_count ============================="<<recv_count<<std::endl;
+				//for(int ii=0; ii<size_recv; ii++)
+				  //  {
+					//if((unsigned int)(data_recv[ii]) != ii) errorCount++;
+					//std::cout << ii << " : "<< (int)(data_recv[ii])<<std::endl;
+				    //}
+				//std::cout << "recved size is ... ... ... : "<< recv_count <<std::endl;
+				//std::cout << " size_recv : "<< size_recv<<std::endl;
+
+			}
+
+
+			size_out -> write(size_recv);
+			data_out -> write(data_recv);
+			//Data sink here
+	    	}   
+	}
+
+
+
+	void notify_sending(){  
+		//std::cout << "notify_sending() time is: " << sc_time_stamp().value() << std::endl;
+
+   		//sent_flag++; 
+		this->sent.notify(); 
+
+
+	}
+	void notify_receiving(char* fileBuffer, unsigned int size){  
+		//std::cout << "notify_receiving() time is: " << sc_time_stamp().value() << std::endl;
+        	data_recv = fileBuffer;
+		size_recv = size;
+		recvd.notify(); 
+	}
+
+
+
+
+
+
+};
+
+
+
+#endif // SC_NIC__H
+
+
