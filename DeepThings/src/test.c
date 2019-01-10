9050c545f5f38f3ecc66b74576846a57b1acc5d6
Zhuoran Zhao <zoranzhao@gmail.com>
2019-01-09 19:29:08 -0600

    Add single device test cases

--- DeepThings/src/test.c
+++ DeepThings/src/test.c
@@ -1,58 +1,82 @@
 #include "darkiot.h"
-#include "config.h"
 #include "configure.h"
+#include "cmd_line_parser.h"
 #include "ftp.h"
 #include "inference_engine_helper.h"
 #include "frame_partitioner.h"
 #include "reuse_data_serialization.h"
-#include <string.h>
 #include "deepthings_edge.h"
 #include "deepthings_gateway.h"
 
-void local_ftp(int argc, char **argv){
-   this_cli_id = 0;
-   total_cli_num = 1;
-   init_queues(total_cli_num);
-   cnn_model* model = load_cnn_model((char*)"models/yolo.cfg", (char*)"models/yolo.weights");
-   model->ftp_para = preform_ftp(3, 3, 4, model->net_para);
+static const char* addr_list[MAX_EDGE_NUM] = EDGE_ADDR_LIST;
 
-   for(int frame_seq = 0; frame_seq < 4; frame_seq++){
-      image_holder img = load_image_as_model_input(model, frame_seq);
-      partition_and_enqueue(model, frame_seq);
-      blob* temp;
-      blob* result;
-      while(1){
-         temp = try_dequeue(task_queue);
-         if(temp == NULL) break;
-         printf("Task id is %d\n", temp->id);
-         set_model_input(model, (float*)temp->data);
-         forward_partition(model, get_blob_task_id(temp), 
-                         model->ftp_para_reuse->schedule[get_blob_task_id(temp)] == 1 && is_reuse_ready(model->ftp_para_reuse, get_blob_task_id(temp))
-                         );  
-         result = new_blob_and_copy_data(0, 
+void process_task_one_device(device_ctxt* ctxt, blob* temp, bool is_reuse){
+   cnn_model* model = (cnn_model*)(ctxt->model);
+   blob* result;
+   set_model_input(model, (float*)temp->data);
+   forward_partition(model, get_blob_task_id(temp), is_reuse);  
+   result = new_blob_and_copy_data(0, 
                                       get_model_byte_size(model, model->ftp_para->fused_layers-1), 
                                       (uint8_t*)(get_model_output(model, model->ftp_para->fused_layers-1))
                                      );
-         copy_blob_meta(result, temp);
-         enqueue(results_pool[this_cli_id], result);
-         free_blob(result);
+#if DATA_REUSE
+   //send_reuse_data(ctxt, temp);
+   
+#endif
+   copy_blob_meta(result, temp);
+   enqueue(ctxt->result_queue, result); 
+   free_blob(result);
+}
+
+void partition_frame_and_perform_inference_thread(void *arg){
+   device_ctxt* ctxt = (device_ctxt*)arg;
+   cnn_model* model = (cnn_model*)(ctxt->model);
+#ifdef NNPACK
+   nnp_initialize();
+   model->net->threadpool = pthreadpool_create(THREAD_NUM);
+#endif
+   blob* temp;
+   uint32_t frame_num;
+   bool* reuse_data_is_required;   
+   for(frame_num = 0; frame_num < FRAME_NUM; frame_num ++){
+      /*Wait for i/o device input*/
+      /*recv_img()*/
+      /*Load image and partition, fill task queues*/
+      load_image_as_model_input(model, frame_num);
+      partition_and_enqueue(ctxt, frame_num);
+
+      /*Dequeue and process task*/
+      while(1){
+         temp = try_dequeue(ctxt->task_queue);
+         if(temp == NULL) break;
+         bool data_ready = false;
+         process_task(ctxt, temp, data_ready);
          free_blob(temp);
       }
-
-      enqueue(ready_pool, new_empty_blob(this_cli_id));
-      temp = (dequeue_and_merge(model));
-      float* fused_output = (float* )temp->data;
-      set_model_input(model, fused_output);
-      forward_all(model, model->ftp_para->fused_layers);   
-      free(fused_output);
-      draw_object_boxes(model, frame_seq);
-      free_image_holder(model, img);
    }
+#ifdef NNPACK
+   pthreadpool_destroy(model->net->threadpool);
+   nnp_deinitialize();
+#endif
 }
 
 
 int main(int argc, char **argv){
-   local_ftp(argc, argv);
+   /*Initialize the data structure and network model*/
+   uint32_t total_cli_num = get_int_arg(argc, argv, "-total_edge", 1);
+   uint32_t this_cli_id = get_int_arg(argc, argv, "-edge_id", 0);
+
+   uint32_t partitions_h = get_int_arg(argc, argv, "-n", 5);
+   uint32_t partitions_w = get_int_arg(argc, argv, "-m", 5);
+   uint32_t fused_layers = get_int_arg(argc, argv, "-l", 16);
+
+   char network_file[30] = "models/yolo.cfg";
+   char weight_file[30] = "models/yolo.weights";
+
+   device_ctxt* client_ctxt = deepthings_edge_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, this_cli_id);
+   device_ctxt* gateway_ctxt = deepthings_gateway_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, total_cli_num, addr_list);
+
+
    return 0;
 }
 
