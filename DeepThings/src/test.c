f70f2034b1d82ebbfd35f388e1f7d8729df1ab4e
Zhuoran Zhao <zoranzhao@gmail.com>
2019-01-10 22:57:58 -0600

    I am stuck

--- DeepThings/src/test.c
+++ DeepThings/src/test.c
@@ -11,6 +11,7 @@
 static const char* addr_list[MAX_EDGE_NUM] = EDGE_ADDR_LIST;
 
 void process_task_single_device(device_ctxt* ctxt, blob* temp, bool is_reuse){
+   printf("Task is: %d, frame number is %d\n", get_blob_task_id(temp), get_blob_frame_seq(temp));
    cnn_model* model = (cnn_model*)(ctxt->model);
    blob* result;
    set_model_input(model, (float*)temp->data);
@@ -79,6 +80,21 @@ void transfer_data(device_ctxt* client, device_ctxt* gateway){
 
 void deepthings_merge_result_thread(void *arg);
 
+device_ctxt* deepthings_edge_init_single_device(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id){
+   device_ctxt* ctxt = init_client(edge_id);
+   cnn_model* model = load_cnn_model(network, weights);
+   model->ftp_para = preform_ftp(N, M, fused_layers, model->net_para);
+#if DATA_REUSE
+   model->ftp_para_reuse = preform_ftp_reuse(model->net_para, model->ftp_para);
+#endif
+   ctxt->model = model;
+   set_gateway_local_addr(ctxt, GATEWAY_LOCAL_ADDR);
+   set_gateway_public_addr(ctxt, GATEWAY_PUBLIC_ADDR);
+   set_total_frames(ctxt, FRAME_NUM);
+   set_batch_size(ctxt, N*M);
+
+   return ctxt;
+}
 
 int main(int argc, char **argv){
    /*Initialize the data structure and network model*/
@@ -92,14 +108,14 @@ int main(int argc, char **argv){
    char network_file[30] = "models/yolo.cfg";
    char weight_file[30] = "models/yolo.weights";
 
-   device_ctxt* client_ctxt = deepthings_edge_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, this_cli_id);
-   device_ctxt* gateway_ctxt = deepthings_gateway_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, total_cli_num, addr_list);
+   device_ctxt* client_ctxt = deepthings_edge_init_single_device(partitions_h, partitions_w, fused_layers, network_file, weight_file, this_cli_id);
+   //device_ctxt* gateway_ctxt = deepthings_gateway_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, total_cli_num, addr_list);
+   partition_frame_and_perform_inference_thread_single_device(client_ctxt);
+   //sys_thread_t t1 = sys_thread_new("partition_frame_and_perform_inference_thread_single_device", 
+                        //             partition_frame_and_perform_inference_thread_single_device, client_ctxt, 0, 0);
+   //sys_thread_t t2 = sys_thread_new("deepthings_merge_result_thread", deepthings_merge_result_thread, gateway_ctxt, 0, 0);
 
-   sys_thread_t t1 = sys_thread_new("partition_frame_and_perform_inference_thread_single_device", 
-                                     partition_frame_and_perform_inference_thread_single_device, client_ctxt, 0, 0);
-   sys_thread_t t2 = sys_thread_new("deepthings_merge_result_thread", deepthings_merge_result_thread, gateway_ctxt, 0, 0);
-
-   transfer_data(client_ctxt, gateway_ctxt);
+   //transfer_data(client_ctxt, gateway_ctxt);
    
 
    return 0;
