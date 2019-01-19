20ec7f51e35d4bc7100b535db95b266d8cf3416a
Zhuoran Zhao <zoranzhao@gmail.com>
2019-01-19 17:42:58 -0600

    ... ...

--- DeepThings/port/example/AppTasks.h
+++ DeepThings/port/example/AppTasks.h
@@ -22,10 +22,19 @@ void test(void* arg){
    device_ctxt* client_ctxt = deepthings_edge_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, this_cli_id);
    device_ctxt* gateway_ctxt = deepthings_gateway_init(partitions_h, partitions_w, fused_layers, network_file, weight_file, total_cli_num, addr_list);
 
-   /*Single-thread version*/
-   partition_frame_and_perform_inference_thread_single_device(client_ctxt);
-   transfer_data_with_number(client_ctxt, gateway_ctxt, FRAME_NUM*partitions_h*partitions_h);
-   deepthings_merge_result_thread_single_device(gateway_ctxt);
+   //partition_frame_and_perform_inference_thread_single_device(client_ctxt);
+   //transfer_data_with_number(client_ctxt, gateway_ctxt, FRAME_NUM*partitions_h*partitions_h);
+   //deepthings_merge_result_thread_single_device(gateway_ctxt);
+   
+   sys_thread_t t1 = sys_thread_new("partition_frame_and_perform_inference_thread_single_device",
+                                     partition_frame_and_perform_inference_thread_single_device, client_ctxt, 49, 0);
+   sys_thread_t t2 = sys_thread_new("deepthings_merge_result_thread_single_device", deepthings_merge_result_thread_single_device, gateway_ctxt, 49, 1);
+   transfer_data(client_ctxt, gateway_ctxt);
+   sys_thread_join(t1);
+   sys_thread_join(t2);
+
+
+
 
 }
 
@@ -86,8 +95,8 @@ private:
       sim_ctxt.register_task(os_ctxt, app_ctxt, os_task_id, sc_core::sc_get_current_process_handle());
        
       if(NodeID==0)
-	 test(NULL);
          //sys_thread_new("test", test, NULL, 49, 0);
+	 test(NULL);
       os_port->taskTerminate(os_task_id);
    }
 };
--- DeepThings/src/test_utils.h
+++ DeepThings/src/test_utils.h
@@ -4,10 +4,8 @@
 #include "configure.h"
 #include "cmd_line_parser.h"
 #include "frame_partitioner.h"
-
-/*Functions defined in the DeepThings library*/
-device_ctxt* deepthings_edge_init(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t edge_id);
-device_ctxt* deepthings_gateway_init(uint32_t N, uint32_t M, uint32_t fused_layers, char* network, char* weights, uint32_t total_edge_number, const char** addr_list);
+#include "deepthings_edge.h"
+#include "deepthings_gateway.h"
 
 /*Functions defined for testing and profiling*/
 void process_task_single_device(device_ctxt* ctxt, blob* temp, bool is_reuse);
