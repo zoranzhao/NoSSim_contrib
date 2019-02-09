78a4ec18e0037794138a5643a2d3e287f2d17ba7
Zhuoran Zhao <zoranzhao@gmail.com>
2019-02-09 15:16:27 -0600

    Add debugging information

--- OMNeT_DeepThings_HCSim/src/NoS/applications/OSNode/global.cc
+++ OMNeT_DeepThings_HCSim/src/NoS/applications/OSNode/global.cc
@@ -1,12 +1,11 @@
 #include <systemc.h>
-
+#include "lwip_ctxt.h"
 double power_cli[10];
 double CliEnergy[10];
-unsigned char debug_flags;
+unsigned char debug_flags=LWIP_DBG_OFF;
 int node_choice=0;
 
-//unsigned char debug_flags=LWIP_DBG_ON;
-//unsigned char debug_flags;
+
 int sc_main(int, char *[]){
 
     std::cout << "The sc_main should not be called ..." <<std::endl;
